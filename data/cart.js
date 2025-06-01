9c242c145b2fd5a5b1acc2a11494a3c8526b8fbb
asadiqb <asadiqbg2000@gmail.com>
2025-06-01 16:12:41 +0500

    delete button interaction i.e remove from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
@@ -23,4 +23,17 @@ export function addToCart(productId){
       quantity: 1
     });
     }
+}
+
+export function removeFromCart(productId){
+  const newCart = [];
+
+  cart.forEach((cartItem)=>{
+    //the result of this loop is that it will contain all the cart item except the one we want to remove i.e productId so it will check through the cart array and if the items in not equal(!==) to the one we want to remove it will push them to new array
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
