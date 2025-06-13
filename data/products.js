c70a3347175c3687862c6e1b280e0cd7dfd2f212
asadiqb <asadiqbg2000@gmail.com>
2025-06-13 14:33:07 +0500

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -9,7 +9,28 @@ export function getProduct(productId){
   return matchingProduct;
 }
 
-export const products = [
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
+
+  constructor(productDetails){
+    this.id =productDetails.id;
+    this.image =productDetails.image;
+    this.name = productDetails.name;
+   this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+}
+ export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -668,4 +689,7 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+  return new Product(productDetails)
+});
+console.log(products);
